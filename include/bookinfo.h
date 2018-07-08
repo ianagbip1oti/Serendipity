28a5d093f25fa15da3ffc1ef57c4a62d3870de1a
Unknown <mythologyali@gmail.com>
2018-07-08 01:35:02 -0700

    added Book class

--- include/bookinfo.h
+++ include/bookinfo.h
@@ -1,1 +1,47 @@
 #pragma once
+#include <string>
+
+
+class Book {
+    /**
+     * Don't want to stealthily include things
+     * in the namespace of files that import this
+     * header
+     */
+    using std::string;
+
+    string title;
+    string isbn;
+    string author;
+    string publisher;
+    string dateAdded;
+    double wholesaleCost;
+    double retailPrice;
+    int quantity;
+public:
+    void setTitle(string);
+    std::string getTitle();
+
+    void setISBN(string);
+    std::string getISBN();
+
+    void setAuthor(string);
+    std::string getAuthor();
+
+    void setPublisher(string);
+    std::string getPublisher();
+
+    void setDateAdded(string);
+    std::string getDateAdded();
+
+    void setWholesaleCost(double);
+    double getWholesaleCost();
+
+    void setRetailPrice(double);
+    double getRetailPrice();
+
+    void setQuantity(int);
+    int getQuantity();
+
+    void print();
+};
--- src/bookinfo.cpp
+++ src/bookinfo.cpp
@@ -1,1 +1,85 @@
+#include "bookinfo.h"
+#include <string>
+#include <iostream>
+
+void Book::setTitle(std::string newName) {
+    title = newName;
+}
+
+std::string Book::getTitle() {
+    return title;
+}
+
+void Book::setISBN(std::string newISBN) {
+
+    isbn = newISBN;
+}
+
+std::string Book::getISBN() {
+    return isbn;
+}
+
+void Book::setAuthor(std::string newAuthor) {
+    author = newAuthor;
+}
+
+std::string Book::getAuthor(){
+    return author;
+}
+
+void Book::setPublisher(std::string newPublisher) {
+    publisher = newPublisher;
+}
+
+std::string Book::getPublisher() {
+    return publisher;
+}
+
+
+void Book::setDateAdded(std::string newDate) {
+    dateAdded = newDate;
+}
+
+std::string Book::getDateAdded() {
+    return dateAdded;
+}
+
+void Book::setQuantity(int newAmount) {
+    quantity = newAmount;
+}
+
+int Book::getQuantity() {
+    return quantity;
+}
+
+void Book::setWholesaleCost(double newCost) {
+    wholesaleCost = newCost;
+}
+
+double Book::getWholesaleCost() {
+    return wholesaleCost;
+}
+
+void Book::setRetailPrice(double newPrice) {
+    retailPrice = newPrice;
+}
+
+double Book::getRetailPrice(){
+    return retailPrice;
+}
+
+void Book::print() {
+    std::cout << "           Title: " << getTitle() << std::endl
+              << "            ISBN: " << getISBN() << std::endl
+              << "          Author: " << getAuthor() << std::endl
+              << "       Publisher: " << getPublisher() << std::endl
+              << "      Date Added: " << getDateAdded() << std::endl
+              << "Quantity on hand: " << getQuantity() << std::endl
+              << "  Wholesale Cost: $" << getWholesaleCost() << std::endl
+              << "    Retail Price: $" << getRetailPrice() << std::endl;
+}
+
+
+
+
 
